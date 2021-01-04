90dd5df8571f6d0d9fd1b85da5a01d6e0526e798
ABISHEKRAJ R <56864775+PrinceofChum@users.noreply.github.com>
2021-01-04 17:00:15 +0530

    Create YaarAnthaPonnu.py

---
+++ YaarAnthaPonnu.py
@@ -0,0 +1,7 @@
+def YaarAnthaPonnu(She):
+    if She is Cute and Charming:
+        if She is Topper and Intelligent:
+            if She is Short and VeryGoodCharacter:
+                return She is Programmer 
+
+print(YaarAnthaPonnu())
