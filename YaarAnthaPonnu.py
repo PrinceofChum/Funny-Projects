547c67c30319335a6a07115aefcbe9c5f964bbf9
ABISHEKRAJ R <abhibalafriends@gmail.com>
2021-01-16 12:38:43 +0530

    FLAMES updated

--- YaarAnthaPonnu.py
+++ YaarAnthaPonnu.py
@@ -10,9 +10,17 @@ def flames(name1,name2):
             count+=1
     finc=total-(2*count)
     
-    #Simple method
+
     while len(flame)>1:
-            del flame[finc%len(flame)-1] 
+        flamer=(finc%len(flame))-1
+        
+        if flamer>=0:
+            right=flame[flamer+1:]
+            left=flame[:flamer]
+            flame=right+left
+        else:
+            flame=flame[:len(flame)-1]
+
 
     print ('\n',*flame,'\n')
 
